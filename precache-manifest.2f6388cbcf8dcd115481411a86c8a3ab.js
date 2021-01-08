36fbc43b93473e7164d58dad94a51d694ccadca5
hlorellium <tdoregani@hotmail.com>
2021-01-08 23:29:34 +0300

    Updates

---
+++ precache-manifest.2f6388cbcf8dcd115481411a86c8a3ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b1789124cc5b2abaf5252af2125888",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "ea25e870ddd60cc72e27",
+    "url": "/react-quiz/static/js/2.42104faa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.42104faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78dcb41d65a9dc8edafe",
+    "url": "/react-quiz/static/js/main.56829a20.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  },
+  {
+    "revision": "5da212d4a23f81b067ed4a5066ead963",
+    "url": "/react-quiz/static/media/tyler-devine.5da212d4.jpg"
+  }
+]);
